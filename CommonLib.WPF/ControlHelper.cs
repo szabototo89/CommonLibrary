796e47acfe011ee64c6d71b0de7c768e1780c800
szabototo89 <szabototo89@gmail.com>
2013-04-01 20:04:08 +0200

    New WPF Common Library Project

--- CommonLib.WPF/ControlHelper.cs
+++ CommonLib.WPF/ControlHelper.cs
@@ -10,27 +10,15 @@ namespace CommonLib.WPF
 {
 	public class ControlHelper
 	{
-		public static IEnumerable<DependencyObject> GetVisualChildren(DependencyObject reference, bool isRecursive = false)
+		public static IEnumerable<DependencyObject> GetVisualChildren(DependencyObject reference)
 		{
 			if (reference == null)
 				throw new ArgumentNullException("reference");
 			var count = VisualTreeHelper.GetChildrenCount(reference);
 
-			if (!isRecursive)
-			{
-				for (var i = 0; i < count; i++)
-					yield return VisualTreeHelper.GetChild(reference, i);
-			}
-			else
-			{
-				if (count == 0)
-					yield return reference;
-				for (var i = 0; i < count; i++)
-				{
-					foreach (var child in GetVisualChildren(VisualTreeHelper.GetChild(reference, i), true))
-						yield return child;
-				}
-			}
+			for (var i = 0; i < count; i++)
+				yield return VisualTreeHelper.GetChild(reference, i);
+
 		}
 	}
 }
